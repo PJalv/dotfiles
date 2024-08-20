a95b4107534390a500fd0286bb6093b29a858325
PJalv <pjalvbusiness@gmail.com>
2024-08-20 12:45:51 -0700

    add file specific theming

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+--
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    if vim.bo.filetype == "zig" or vim.bo.filetype == "c" or vim.bo.filetype == "cpp" then
+      vim.cmd.colorscheme("gruvbox")
+      require("lualine").setup({ options = { theme = "gruvbox" } })
+    else
+      vim.cmd.colorscheme("tokyonight")
+      require("lualine").setup({ options = { theme = "tokyonight" } })
+    end
+  end,
+})
