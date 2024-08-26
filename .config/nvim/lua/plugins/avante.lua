392875f85288b0fcb033fc357740a37d9c5b741c
PJalv <pjalvbusiness@gmail.com>
2024-08-25 20:58:55 -0700

    tweak avante

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -4,10 +4,20 @@ return {
   build = "make",
   opts = {
     provider = "openai",
+    openai = {
+      api_key_name = "cmd:cat " .. os.getenv("HOME") .. "/theenv/openaiapi",
+      model = "gpt-4o-mini",
+    },
     -- add any opts here
+    windows = {
+      sidebar_header = {
+        rounded = false,
+      },
+    },
   },
   dependencies = {
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "echasnovski/mini.icons",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
