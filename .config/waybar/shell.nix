16c60f3fe552508516df75dfea5cb727d9699a8d
PJalv <pjalvbusiness@gmail.com>
2025-03-06 09:59:22 -0800

    tweak

--- .config/waybar/shell.nix
+++ .config/waybar/shell.nix
@@ -11,7 +11,7 @@ pkgs.mkShell rec {
   shellHook = ''
     export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
     export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
-
+    uv venv
     source .venv/bin/activate
     uv pip install realtimestt
 
